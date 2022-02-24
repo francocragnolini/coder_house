4278aaa21e2539879d8455b316d29d6c1cd688b2
Franco Cragnolini <cragnolinifranco@gmail.com>
2022-02-24 19:01:33 -0400

    added categories and filtering

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,28 @@
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import CartWidget from "./CartWidget";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const NavBar = () => {
+  const [categories, setCategories] = useState([]);
+
+  // fetching the array of categories
+  useEffect(() => {
+    const getCategories = async () => {
+      const query = collection(db, "categories");
+      const snapshot = await getDocs(query);
+      const listItems = [];
+      snapshot.forEach((doc) => {
+        const object = { id: doc.id, ...doc.data() };
+        return listItems.push(object);
+      });
+      setCategories(listItems);
+    };
+    getCategories();
+  }, []);
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>
@@ -16,17 +36,12 @@ const NavBar = () => {
       </div>
 
       <ul className={styles.list}>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <NavLink to={`/category/${category.name}`}>{category.name}</NavLink>
+          </li>
+        ))}
         <li>
-          <NavLink to={`/category/categoryId`}>Suits</NavLink>
-        </li>
-        <li>
-          <NavLink to={`/category/categoryId`}>Shirts</NavLink>
-        </li>
-        <li>
-          <NavLink to={`/category/categoryId`}>Shoes</NavLink>
-        </li>
-        <li>
-          {}
           <NavLink to="/cart">
             <CartWidget />
           </NavLink>
--- src/pages/ItemListContainer.jsx
+++ src/pages/ItemListContainer.jsx
@@ -1,13 +1,19 @@
 import classes from "./ItemListContainer.module.css";
 import ItemList from "../components/ItemList";
 import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { useParams, useHistory } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 
 const ItemListContainer = (props) => {
   // simulates fetching an Api
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
+  const param = useParams().id;
+  const pathName = useHistory().location.pathname;
+
+  // fetching all Products
   useEffect(() => {
     const getProducts = async () => {
       const query = collection(db, "items");
@@ -22,9 +28,31 @@ const ItemListContainer = (props) => {
     getProducts();
   }, []);
 
+  // fetching filtered Products by category
+  useEffect(() => {
+    const getFilteredProducts = async () => {
+      const q = query(
+        collection(db, "items"),
+        where("categoryId", "==", `${param}`)
+      );
+      const snapshot = await getDocs(q);
+      const listItems = [];
+      snapshot.forEach((doc) => {
+        const object = { id: doc.id, ...doc.data() };
+        return listItems.push(object);
+      });
+      setFilteredProducts(listItems);
+    };
+    getFilteredProducts();
+  }, [param]);
+
   return (
     <div className={classes["item-list-container"]}>
-      <ItemList items={products} />
+      {pathName === "/" ? (
+        <ItemList items={products} />
+      ) : (
+        <ItemList items={filteredProducts} />
+      )}
     </div>
   );
 };
